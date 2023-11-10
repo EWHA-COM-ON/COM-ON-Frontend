82b5da2f0c3416a1ded49ff55e236bdb9e347fe6
SujinRo <shtnwls1111@ewhain.net>
2023-11-11 03:18:55 +0900

    feat: prettier 설정, font 설정, global css 설정

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+
+export default function Login() {
+
+
+  return (
+     <Container>
+      
+     </Container>
+  )
+}
---
+++ src/app/styles/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  //color
+  mainGrey: '363636',
+  subGrey: '686868',
+  mainGreen: '#00462A',
+  mainWhite: '#FFFFFF',
+};
