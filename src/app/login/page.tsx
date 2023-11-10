b61f5d2cd17a99ced0c40afc9d2b55ed32879058
SujinRo <shtnwls1111@ewhain.net>
2023-11-11 04:48:24 +0900

    fix: style 방법_styled-components => css module로 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,11 @@
-import styled from 'styled-components';
+import styles from '@/app/styles/login.module.css';
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.title}>로그인</div>
+      </div>
+    </div>
+  );
 }
--- src/app/styles/theme.ts
+++
@@ -1,7 +0,0 @@
-export const theme = {
-  //color
-  mainGrey: '363636',
-  subGrey: '686868',
-  mainGreen: '#00462A',
-  mainWhite: '#FFFFFF',
-};
