54e65d29bcafe12b861c757af6f829241234d4b8
SujinRo <shtnwls1111@ewhain.net>
2023-11-11 15:21:44 +0900

    feat: input 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,16 @@
 import styles from '@/app/styles/login.module.css';
+import { useState } from 'react';
 
 export default function Login() {
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         <div className={styles.title}>로그인</div>
+        <input type="text" id="id" />
+        <input type="text" id="pwd" />
       </div>
     </div>
   );
