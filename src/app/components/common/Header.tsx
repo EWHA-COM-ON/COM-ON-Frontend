861c03d7f8bedae7dee13e5c9f48d28044674984
sujinRo <shtnwls1111@ewhain.net>
2023-11-28 01:04:30 +0900

    feat: 모니터 크기 일 때, header 완료

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -1,5 +1,37 @@
+'use client'
 import styles from '@/app/styles/header.module.css';
+import fonts from '@/app/styles/fonts.module.css';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
-  return <div className={styles.container}></div>;
+  const pathname = usePathname();
+
+  const barList = [
+    { id: 0, name: '학생회 소개', pathname: '/' },
+    { id: 1, name: '학생회 정보', pathname: '/' },
+    { id: 2, name: '질문 게시판', pathname: '/' },
+    { id: 3, name: '행사 신청', pathname: '/' },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <Link href = {'/'} className={styles.logo}>
+      <img src='https://i.ibb.co/SNGMqNq/logo.png' width={234} height={44} alt="" />
+      </Link>
+      <div className={styles.barList}>
+        {barList.map(bar => (
+          <div className={styles.barSet}>
+          <Link href={bar.pathname} key={bar.id}>
+            <div className={`${styles.bar} ${fonts.bar}`}>{bar.name}</div>
+          </Link>
+          {pathname === bar.pathname?<div className={styles.line}></div>:<></>}
+          </div>
+        ))}
+      </div>
+      <div className={`${styles.loginBtn} ${fonts.router}`}>
+        <Link href={'/login'} className={styles.btn}>로그인</Link>/<Link href={'/join'} className={styles.btn}>회원가입</Link>
+      </div>
+    </div>
+  );
 }
