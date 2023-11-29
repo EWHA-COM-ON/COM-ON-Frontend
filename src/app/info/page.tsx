86842ba6ca59085e9fb9b3dd9958dfab55e2dc6b
sujinRo <shtnwls1111@ewhain.net>
2023-11-30 03:58:53 +0900

    feat: info page, intro page UX 완료

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -2,7 +2,20 @@ import styles from '../styles/info.module.css';
 import fonts from '../styles/fonts.module.css';
 
 export default function Info() {
-  <div className={styles.container}>
-    <div className={styles.box}></div>
-  </div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.sectionBox}>
+          <div className={`${styles.title} ${fonts.title}`}>학생회 공약</div>
+          <div className={styles.line} />
+          <div className={`${styles.textBox} ${fonts.placeholder}`}>안녕하세요.</div>
+        </div>
+        <div className={styles.sectionBox}>
+          <div className={`${styles.title} ${fonts.title}`}>학생회 부원</div>
+          <div className={styles.line} />
+          <div className={`${styles.textBox} ${fonts.placeholder}`}>안녕하세요.</div>
+        </div>
+      </div>
+    </div>
+  );
 }
