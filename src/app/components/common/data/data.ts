dedbcb7a2cb548362637c710e724c6020c026467
sujinRo <shtnwls1111@ewhain.net>
2023-11-29 22:58:51 +0900

    feat: info 화면 하는중

--- src/app/components/common/data/data.ts
+++ src/app/components/common/data/data.ts
@@ -1,6 +1,6 @@
 export const barList = [
-  { id: 0, name: '학생회 소개', pathname: '/' },
-  { id: 1, name: '학생회 정보', pathname: '/a' },
-  { id: 2, name: '질문 게시판', pathname: '/b' },
-  { id: 3, name: '행사 신청', pathname: '/c' },
+  { id: 0, name: '학생회 소개', pathname: '/intro' },
+  { id: 1, name: '학생회 정보', pathname: '/info' },
+  { id: 2, name: '질문 게시판', pathname: '/' },
+  { id: 3, name: '행사 신청', pathname: '/' },
 ];
---
+++ src/app/info/page.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/info.module.css';
+import fonts from '../styles/fonts.module.css';
+
+export default function Info() {
+  <div className={styles.container}>
+    <div className={styles.box}></div>
+  </div>;
+}
