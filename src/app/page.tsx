e97a24f0110cc732550f585fae1ffe34d23a080c
sujinRo <shtnwls1111@ewhain.net>
2023-11-30 04:18:01 +0900

    design: home page event box css 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,22 @@ export default function Home() {
       <div className={styles.bannerBox}></div>
       <div className={styles.wrapBox}>
         <Link href={'https://cse.ewha.ac.kr/cse/index.do'} className={styles.btnBox}>
-          <div className={`${fonts.bar} ${styles.routerText}`}>
+          <div className={`${fonts.bar} ${styles.greyText}`}>
             이화여자대학교 컴퓨터공학전공 홈페이지
           </div>
           <div className={`${fonts.smallM} ${styles.routerSubText}`}>바로가기 </div>
         </Link>
         <Link href={'https://www.ewha.ac.kr/ewha/index.do'} className={styles.btnBox}>
-          <div className={`${fonts.bar} ${styles.routerText}`}>이화여자대학교 홈페이지</div>
+          <div className={`${fonts.bar} ${styles.greyText}`}>이화여자대학교 홈페이지</div>
           <div className={`${fonts.smallM} ${styles.routerSubText}`}>바로가기 </div>
         </Link>
       </div>
       <div className={width >= 738 ? styles.event : styles.smallerEvent}>
-        <div className={`${styles.eventTitle} ${fonts.bar}`}>행사 일정</div>
-        <div className={styles.eventBox}></div>
+        <div className={`${styles.greyText} ${fonts.bar}`}>행사 일정</div>
+        <div className={styles.eventBox}>
+          <div className={`${fonts.smallM} ${styles.greyText}`}>1월</div>
+          <div className={`${fonts.placeholder} ${styles.eventText}`}>졸업식</div>
+        </div>
       </div>
     </div>
   );
