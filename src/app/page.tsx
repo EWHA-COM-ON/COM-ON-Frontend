56288406924582088f4efde36757fb9ecddc273f
sujinRo <shtnwls1111@ewhain.net>
2023-11-30 04:22:42 +0900

    feat: link 새 창에서 나오도록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,21 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.bannerBox}></div>
       <div className={styles.wrapBox}>
-        <Link href={'https://cse.ewha.ac.kr/cse/index.do'} className={styles.btnBox}>
+        <Link
+          href={'https://cse.ewha.ac.kr/cse/index.do'}
+          className={styles.btnBox}
+          target="_blank"
+        >
           <div className={`${fonts.bar} ${styles.greyText}`}>
             이화여자대학교 컴퓨터공학전공 홈페이지
           </div>
           <div className={`${fonts.smallM} ${styles.routerSubText}`}>바로가기 </div>
         </Link>
-        <Link href={'https://www.ewha.ac.kr/ewha/index.do'} className={styles.btnBox}>
+        <Link
+          href={'https://www.ewha.ac.kr/ewha/index.do'}
+          className={styles.btnBox}
+          target="_blank"
+        >
           <div className={`${fonts.bar} ${styles.greyText}`}>이화여자대학교 홈페이지</div>
           <div className={`${fonts.smallM} ${styles.routerSubText}`}>바로가기 </div>
         </Link>
